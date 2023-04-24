ca74d53b033d9a38fbe912a9827b3bac186f505f
Elena Smurova <lllleeenna@yandex.ru>
2023-04-24 12:18:14 +0700

    Исправлены ошибки валидации, добавление workflow

--- backend/foodgram/recipes/validators.py
+++ backend/foodgram/recipes/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 def validate_amount(value):
     if value <= 0:
         raise ValidationError(
-            'Количество ингредиентов не может быть меньше или равно нулю.',
-            params={'value', value}
+            'Коичество ингредиентов не может быть меньше/равным: %(value)s',
+            params={'value': value}
         )
