2b472811c7abe560b39f26239e12ed89a5e51be1
Elena Smurova <lllleeenna@yandex.ru>
2023-04-28 08:55:36 +0700

    flake8

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -21,12 +21,5 @@ class RecipeFilter(django_filters.FilterSet):
         model = Recipe
         fields = ('author', 'tags')
 
-    def filter_is_favorited(self, queryset, name, value):
-        return queryset.filter(favorites__user=self.request.user)
-
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        return queryset.filter(shoppingcarts__user=self.request.user)
-
-
 class IngredientSearchFilter(filters.SearchFilter):
     search_param = 'name'
